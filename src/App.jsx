7b8f8b7d021a3fb6ec9790a6546c3976f301342a
emmytumwine <emmytumwine73@gmail.com>
2024-08-16 11:58:35 +0200

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
+import React from 'react';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import UserProfile from './components/UserProfile';
-
 
 function App() {
   const [count, setCount] = useState(0);
